401a89d8ae87ce81375cdd127fa6fe52609b214f
Vlad Ghimboasa <vladalexandrughimboasa@gmail.com>
2020-01-21 21:19:36 -0500

    Premier commit

--- labo/src/tp1/Point2d.java
+++ labo/src/tp1/Point2d.java
@@ -6,12 +6,12 @@ public class Point2d extends AbstractPoint {
 
     // TODO creer un point en 2d avec 2 donnees
     public Point2d(Double x, Double y) {
-        super(null /*...*/);
+        super(new Double[] {x, y});
     }
 
     // TODO creer un point a partir d'un vecteur de donnees
     public Point2d(Double[] vector) {
-        super(null /*...*/);
+        super(new Double[]{vector[0], vector[1]});
     }
 
     public Double X() { return vector[X];}
@@ -20,46 +20,46 @@ public class Point2d extends AbstractPoint {
     // TODO prendre un vecteur de donnees et appliquer la translation.
     @Override
     public Point2d translate(Double[] translateVector) {
-        return null;
+        return new Point2d(PointOperator.translate(vector, translateVector));
     }
 
     // TODO prendre un point et appliquer la translation.
     public Point2d translate(Point2d translateVector) {
-        return null;
+        return new Point2d(PointOperator.translate(vector, new Double[]{translateVector.X(), translateVector.Y()}));
     }
 
     // TODO prendre un vecteur de donnees et appliquer la translation.
     @Override
     public Point2d rotate(Double[][] rotationMatrix) {
-        return null;
+        return new Point2d(PointOperator.rotate(vector, rotationMatrix));
     }
 
     // TODO prendre un angle de rotation, creer une matrice et appliquer la rotation.
     public Point2d rotate(Double angle) {
-        return null;
+        return new Point2d(PointOperator.rotate(vector, new Double[][]{{Math.cos(angle), -(Math.sin(angle))}, {Math.sin(angle), Math.cos(angle)}}));
     }
 
     // TODO prendre un facteur de division et l'appliquer.
     @Override
     public Point2d divide(Double divider) {
-        return null;
+        return new Point2d(PointOperator.divide(vector, divider));
     }
 
     // TODO prendre un facteur de multiplication et l'appliquer.
     @Override
     public Point2d multiply(Double multiplier) {
-        return null;
+        return new Point2d(PointOperator.multiply(vector, multiplier));
     }
 
     // TODO prendre un facteur d'addition et l'appliquer.
     @Override
     public Point2d add(Double adder) {
-        return null;
+        return new Point2d(PointOperator.add(vector, adder));
     }
 
     // TODO creer un nouveau point.
     @Override
     public Point2d clone() {
-        return this;
+        return new Point2d(this.vector);
     }
 }
--- labo/src/tp1/PointOperator.java
+++ labo/src/tp1/PointOperator.java
@@ -5,36 +5,81 @@ import java.util.*;
 public final class PointOperator {
     // TODO appliquer la translation sur le vecteur d'entree.
     public static Double[] translate(Double[] vector, Double[] translateVector) {
-        return null;
+        for(int i = 0; i < vector.length; i++){
+            vector[i] += translateVector[i];
+        }
+        return vector;
     }
 
     // TODO appliquer la rotation sur le vecteur d'entree.
     public static Double[] rotate(Double[] vector, Double[][] rotationMatrix) {
-        return null;
+
+        Double[] vectorResultant = new Double[vector.length];
+
+        for(int i = 0; i<rotationMatrix.length; i++)
+        {
+            double sum = 0;
+            for(int j = 0; j<rotationMatrix[i].length; j++)
+            {
+                sum += rotationMatrix[i][j] * vector[j];
+            }
+            vectorResultant[i] = sum;
+
+        }
+        return vectorResultant;
     }
 
     // TODO appliquer le facteur de division sur le vecteur d'entree.
     public static Double[] divide(Double[] vector, Double divider) {
-        return null;
+        for(int i = 0; i < vector.length; i++){
+            vector[i] = vector[i] / divider;
+        }
+        return vector;
     }
 
     // TODO appliquer le facteur de multiplication sur le vecteur d'entree.
     public static Double[] multiply(Double[] vector, Double multiplier) {
-        return null;
+        for(int i = 0; i < vector.length; i++){
+            vector[i] = vector[i] * multiplier;
+        }
+        return vector;
     }
 
     // TODO appliquer le facteur d'addition sur le vecteur d'entree.
     public static Double[] add(Double[] vector, Double adder) {
-        return null;
+        for(int i = 0; i < vector.length; i++){
+            vector[i] += adder;
+        }
+        return vector;
     }
 
     // TODO retourne la coordonnee avec les plus grandes valeurs en X et en Y.
     public static Point2d getMaxCoord(Collection<Point2d> coords) {
-        return null;
+        double maxX = 0.0;
+        double maxY = 0.0;
+        for(Point2d pt : coords){
+            if(maxX < pt.X()){
+                maxX = pt.X();
+            }
+            if(maxY < pt.Y()){
+                maxY = pt.Y();
+            }
+        }
+        return new Point2d(maxX, maxY);
     }
 
     // TODO retourne la coordonnee avec les plus petites valeurs en X et en Y.
     public static Point2d getMinCoord(Collection<Point2d> coords) {
-        return null;
+        double minX = 0.0;
+        double minY = 0.0;
+        for(Point2d pt : coords){
+            if(minX > pt.X()){
+                minX = pt.X();
+            }
+            if(minY > pt.Y()){
+                minY = pt.Y();
+            }
+        }
+        return new Point2d(minX, minY);
     }
 }
